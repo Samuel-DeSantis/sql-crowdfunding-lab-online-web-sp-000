59e6272367c67d8cbe27715bb66c695aa387e5d3
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-02-03 02:45:07 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+)
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+)
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user INTEGER,
+  FOREIGN KEY(user) REFERENCES users(id),
+  project INTEGER,
+  FOREIGN KEY(project) REFERENCES projects(id)
+)
